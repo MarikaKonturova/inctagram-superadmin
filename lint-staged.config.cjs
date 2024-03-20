fd0305a08d79095570b475d61d33c6b8a6560b17
MarikaKonturova <mashakonturova@gmail.com>
2024-03-21 00:18:46 +0300

    fix stylelint

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -5,8 +5,10 @@ const eslintCommand = filenames =>
   `next lint --file ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`
 
 const formatCommand = 'prettier --write'
+const stylelintCommand = 'stylelint --allow-empty-input "**/*.css"'
 
 module.exports = {
   '!*.{js,jsx,ts,tsx,css}': [formatCommand],
   '*.{js,jsx,ts,tsx}': [formatCommand, eslintCommand],
+  '*.css': [formatCommand, stylelintCommand],
 }
