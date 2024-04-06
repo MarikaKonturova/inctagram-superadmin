7b2cd974126d676e850e8dbdbb7e6597f0f1ed71
Ruban Pavel <ruban.pavel@outlook.com>
2024-04-06 20:46:19 +0300

    Scrum-342 add: layout

--- src/layouts/layout/Layout.tsx
+++ src/layouts/layout/Layout.tsx
@@ -8,5 +8,11 @@ interface LayoutProps extends PropsWithChildren {
 export const Layout: NextPage<LayoutProps> = props => {
   const { children, withAuth = false } = props
 
-  return <div></div>
+  return (
+    <div>
+      <main className={'h-[calc(100vh-60px)][transition:background-color_0.5s]'}>
+        <div className={'w-full h-full pl-16 m-auto'}>{children}</div>
+      </main>
+    </div>
+  )
 }
