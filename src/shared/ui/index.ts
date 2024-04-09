569bb8e888ee16787e34bc3b83f34e55186352ec
MarikaKonturova <mashakonturova@gmail.com>
2024-04-09 21:29:43 +0300

    add styles from snadcn ui

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,4 @@
-export { Button } from './button/Button'
-export { Info } from './info/Info'
-export { Input } from './input/Input'
-export { Label } from './label/Label'
+export { Button } from './Button'
+export { Info } from './Info'
+export { Input } from './Input'
+export { Label } from './Label'
