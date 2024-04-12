5b41e426e277cc8d0a2bee2f83221e517ab42143
chepeLove <ilya.chepelov@gmail.com>
2024-04-12 13:41:50 +0300

    SCRUM-344 fix: change validation password

--- src/features/auth/lib/loginFormSchema.ts
+++ src/features/auth/lib/loginFormSchema.ts
@@ -2,9 +2,5 @@ import { z } from 'zod'
 
 export const loginSchema = z.object({
   email: z.string().email({ message: 'Email must contain A-Z, a-z , @' }),
-  password: z
-    .string()
-    .regex(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[\]:;<>,.?/~_+\-|=`]).{6,20}$/, {
-      message: 'Password must contain 1-9, a-z, A-Z, and specified symbols',
-    }),
+  password: z.string(),
 })
