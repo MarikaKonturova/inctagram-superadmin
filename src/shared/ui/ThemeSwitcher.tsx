04fd7ba7da8d92f7e2243330d647598453b2610b
Ruban Pavel <ruban.pavel@outlook.com>
2024-04-21 18:43:24 +0300

    Scrum-342 add: logic in the theme switcher

--- src/shared/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import React, { memo, useState } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import DarkIcon from 'shared/assets/icons/general/theme-dark.svg'
 import LightIcon from 'shared/assets/icons/general/theme-light.svg'
 import { Button } from 'shared/ui/Buttons'
@@ -10,33 +10,26 @@ interface ThemeSwitcherProps {
 }
 
 export const ThemeSwitcher = memo(({ className, size = 25 }: ThemeSwitcherProps) => {
-  const [theme, setTheme] = useState('DARK')
+  const [theme, setTheme] = useState('dark')
+
   const toggleTheme = () => {
-    setTheme(prevTheme => (prevTheme === 'DARK' ? 'LIGHT' : 'DARK'))
+    const newTheme = theme === 'dark' ? 'light' : 'dark'
+
+    setTheme(newTheme)
   }
-  /* const { theme, toggleTheme } = useTheme()
 
-
-
-  if (!theme) {
-    return null
-  }*/
+  useEffect(() => {
+    document.documentElement.setAttribute('class', theme)
+  }, [theme])
 
   return (
     <Button
       className={clsx('', {}, [className])}
-      /* theme={}*/
       onClick={toggleTheme}
       style={{ lineHeight: 0 }}
       variant={'clear'}
     >
-      {/* {theme === Theme.DARK ? (
-        <DarkIcon height={size} width={size} />
-      ) : (
-        <LightIcon height={size} width={size} />
-      )}*/}
-
-      {theme === 'DARK' ? (
+      {theme === 'dark' ? (
         <DarkIcon height={size} width={size} />
       ) : (
         <LightIcon height={size} width={size} />
