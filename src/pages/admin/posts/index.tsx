49497d025de2b92af2f9a249812e6a5058b5348a
Sergey <sergbfmv@mail.ru>
2024-05-02 14:45:49 +0300

    SCRUM-359 fix: fix with FSD structure and mock data

--- src/pages/admin/posts/index.tsx
+++ src/pages/admin/posts/index.tsx
@@ -11,7 +11,6 @@ export default function PostsPage() {
 
   const mockImageProfile =
     'https://img.freepik.com/free-photo/beautiful-kitten-with-colorful-clouds_23-2150752960.jpg?t=st=1712914944~exp=1712918544~hmac=0d3c7a7296bbb53cbb9572eba3e371bfd1e9da406a00d3f026286572408d101f&w=740'
-
   const mockUserName = 'URLProfile'
 
   return (
