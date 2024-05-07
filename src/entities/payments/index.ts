a272607a532b34d59f195cfe871100326fd06da1
Ilya Chepelov <ilya.chepelov@gmail.com>
2024-05-07 12:57:12 +0300

    feat: add export

---
+++ src/entities/payments/index.ts
@@ -0,0 +1,7 @@
+export { paymentsColumnsTable } from './lib/paymentsColumnsTable'
+export { PaymentsTable } from './ui/PaymentsTable/PaymentsTable'
+export {
+  useGetAllPaymentsQuery,
+  useCreatedSubscriptionSubscription,
+} from './api/getAllPayments.types'
+export { PaymentsList } from './ui/PaymentsList'
