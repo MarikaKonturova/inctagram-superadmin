f99d1b2ccba6b0684c569158c8b0436f12b0206e
Ilya Chepelov <ilya.chepelov@gmail.com>
2024-05-07 12:53:49 +0300

    feat: add table component for payments

---
+++ src/entities/payments/ui/PaymentsTable/PaymentsTable.tsx
@@ -0,0 +1,22 @@
+import { DataTable } from 'shared/ui'
+
+import { paymentsColumnsTable } from 'entities/payments'
+import type { GetAllPaymentsQuery } from 'entities/payments/api/getAllPayments.types'
+
+interface PaymentsListProps {
+  data?: GetAllPaymentsQuery
+}
+
+export const PaymentsTable = ({ data }: PaymentsListProps) => {
+  const formattedData = data?.paymentsList.items.map(item => ({
+    urlAvatar: item.urlAvatar,
+    userName: item.userName,
+    userId: item.userId,
+    amount: item.amount,
+    typeSubscription: item.typeSubscription,
+    createdAt: item.createdAt,
+    paymentTypeText: item.paymentTypeText,
+  }))
+
+  return <DataTable columns={paymentsColumnsTable} data={formattedData || []} />
+}
