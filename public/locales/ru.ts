aab3a35ef5cd663a4e93116ec14cf33453a3b131
Margarita P <rprivalko@gmail.com>
2024-05-26 17:34:53 +0300

    SCRUM-343 add: add translations to locales

--- public/locales/ru.ts
+++ public/locales/ru.ts
@@ -1,6 +1,22 @@
 import {LocaleType} from "./en";
 
 export const ru: LocaleType = {
-
+    sidebar: {
+        usersList: 'Пользователи',
+        statistics: 'Статистика',
+        payments: 'Платежи',
+        posts: 'Посты',
+    },
+    userProfile: {
+        backToUsersList: 'Назад к списку пользователей',
+        linkToProfile: 'Профиль',
+        profileCreationDate: 'Дата создания аккаунта',
+        tabs: {
+            followers: 'Подписчики',
+            following: 'Подписки',
+            payments: 'Платежи',
+            uploadedPhotos: 'Загруженные фотографии',
+        }
+    }
 }
 
